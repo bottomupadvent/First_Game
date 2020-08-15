44f7b1bceddbd506533dc68f23ab332455bdb1e6
Instincts9o2 <pranavchaudhari97@gmail.com>
2020-08-15 22:45:17 +0530

    Added singleton/autoload

---
+++ autoload/PlayerData.gd
@@ -0,0 +1,27 @@
+extends Node
+
+signal coin_collected
+signal player_died
+signal shake_camera
+
+var deaths: int = 3 setget set_deaths, get_deaths
+var coins: int = 0 setget set_coins, get_coins
+
+func reset():
+    deaths = 3
+    coins = 0
+    
+func set_deaths(value: int) -> void:
+    deaths = value
+    emit_signal("player_died")
+    emit_signal("shake_camera")
+
+func get_deaths() -> int:
+    return deaths
+
+func set_coins(value: int) -> void:
+    coins = value
+    emit_signal("coin_collected")
+
+func get_coins() -> int:
+    return coins
