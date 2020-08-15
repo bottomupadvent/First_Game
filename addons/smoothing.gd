b14bf324695c3788df92fd19700f9794ef396458
Instincts9o2 <pranavchaudhari97@gmail.com>
2020-08-15 22:46:37 +0530

    Smoothing addon (lawnjelly <- github)

--- addons/smoothing.gd
+++ addons/smoothing.gd
@@ -42,24 +42,24 @@ export (int, FLAGS, "enabled", "translate", "basis", "slerp") var flags : int = 
 # call this on e.g. starting a level, AFTER moving the target
 # so we can update both the previous and current values
 func teleport():
-	var temp_flags = flags
-	_SetFlags(SF_TRANSLATE | SF_BASIS)
+    var temp_flags = flags
+    _SetFlags(SF_TRANSLATE | SF_BASIS)
 
-	_RefreshTransform()
-	_m_trPrev = _m_trCurr
+    _RefreshTransform()
+    _m_trPrev = _m_trCurr
 
-	# do one frame update to make sure all components are updated
-	_process(0)
+    # do one frame update to make sure all components are updated
+    _process(0)
 
-	# resume old flags
-	flags = temp_flags
+    # resume old flags
+    flags = temp_flags
 
 func set_enabled(var bEnable : bool):
-	_ChangeFlags(SF_ENABLED, bEnable)
-	_SetProcessing()
+    _ChangeFlags(SF_ENABLED, bEnable)
+    _SetProcessing()
 
 func is_enabled():
-	return _TestFlags(SF_ENABLED)
+    return _TestFlags(SF_ENABLED)
 
 
 
@@ -68,137 +68,137 @@ func is_enabled():
 
 
 func _ready():
-	_m_trCurr = Transform()
-	_m_trPrev = Transform()
+    _m_trCurr = Transform()
+    _m_trPrev = Transform()
 
-	assert(_m_Target != null, "A target must be defined for the Smoothing node to work.")
+    assert(_m_Target != null, "A target must be defined for the Smoothing node to work.")
 
 func set_target(new_value):
-	target = new_value
-	if is_inside_tree():
-		_FindTarget()
+    target = new_value
+    if is_inside_tree():
+        _FindTarget()
 
 func get_target():
-	return target
+    return target
 
 func _set_flags(new_value):
-	flags = new_value
-	# we may have enabled or disabled
-	_SetProcessing()
+    flags = new_value
+    # we may have enabled or disabled
+    _SetProcessing()
 
 func _get_flags():
-	return flags
+    return flags
 
 func _SetProcessing():
-	var bEnable = _TestFlags(SF_ENABLED)
-	if _TestFlags(SF_INVISIBLE):
-		bEnable = false
+    var bEnable = _TestFlags(SF_ENABLED)
+    if _TestFlags(SF_INVISIBLE):
+        bEnable = false
 
-	set_process(bEnable);
-	set_physics_process(bEnable);
-	pass
+    set_process(bEnable);
+    set_physics_process(bEnable);
+    pass
 
 func _enter_tree():
-	# might have been moved
-	_FindTarget()
-	pass
+    # might have been moved
+    _FindTarget()
+    pass
 
 func _notification(what):
-	match what:
-		# invisible turns off processing
-		NOTIFICATION_VISIBILITY_CHANGED:
-			_ChangeFlags(SF_INVISIBLE, is_visible_in_tree() == false)
-			_SetProcessing()
+    match what:
+        # invisible turns off processing
+        NOTIFICATION_VISIBILITY_CHANGED:
+            _ChangeFlags(SF_INVISIBLE, is_visible_in_tree() == false)
+            _SetProcessing()
 
 
 
 func _RefreshTransform():
-	_ClearFlags(SF_DIRTY);
+    _ClearFlags(SF_DIRTY);
 
-	if _HasTarget() == false:
-		return
+    if _HasTarget() == false:
+        return
 
-	_m_trPrev = _m_trCurr
-	_m_trCurr = _m_Target.transform
+    _m_trPrev = _m_trCurr
+    _m_trCurr = _m_Target.transform
 
 
 func _FindTarget():
-	_m_Target = null
-	if target.is_empty():
-		return
+    _m_Target = null
+    if target.is_empty():
+        return
 
-	_m_Target = get_node(target)
+    _m_Target = get_node(target)
 
-	if _m_Target is Spatial:
-		return
+    if _m_Target is Spatial:
+        return
 
-	_m_Target = null
-	#return false
+    _m_Target = null
+    #return false
 
 
 func _HasTarget()->bool:
-	if _m_Target == null:
-		return false
+    if _m_Target == null:
+        return false
 
-	# has not been deleted?
-	if is_instance_valid(_m_Target):
-		return true
+    # has not been deleted?
+    if is_instance_valid(_m_Target):
+        return true
 
-	_m_Target = null
-	return false
+    _m_Target = null
+    return false
 
 
 func _process(_delta):
-	if _TestFlags(SF_DIRTY):
-		_RefreshTransform()
+    if _TestFlags(SF_DIRTY):
+        _RefreshTransform()
 
-	var f = Engine.get_physics_interpolation_fraction()
+    var f = Engine.get_physics_interpolation_fraction()
 
-	var tr : Transform = Transform()
+    var tr : Transform = Transform()
 
-	# translate
-	if _TestFlags(SF_TRANSLATE):
-		var ptDiff = _m_trCurr.origin - _m_trPrev.origin
-		tr.origin = _m_trPrev.origin + (ptDiff * f)
+    # translate
+    if _TestFlags(SF_TRANSLATE):
+        var ptDiff = _m_trCurr.origin - _m_trPrev.origin
+        tr.origin = _m_trPrev.origin + (ptDiff * f)
 
-	# rotate
-	if _TestFlags(SF_BASIS):
-		if _TestFlags(SF_SLERP):
-			tr.basis = _m_trPrev.basis.slerp(_m_trCurr.basis, f)
-		else:
-			tr.basis = _LerpBasis(_m_trPrev.basis, _m_trCurr.basis, f)
+    # rotate
+    if _TestFlags(SF_BASIS):
+        if _TestFlags(SF_SLERP):
+            tr.basis = _m_trPrev.basis.slerp(_m_trCurr.basis, f)
+        else:
+            tr.basis = _LerpBasis(_m_trPrev.basis, _m_trCurr.basis, f)
 
-	transform = tr
+    transform = tr
 
-	pass
+    pass
 
 func _physics_process(_delta):
-	# take care of the special case where multiple physics ticks
-	# occur before a frame .. the data must flow!
-	if _TestFlags(SF_DIRTY):
-		_RefreshTransform()
+    # take care of the special case where multiple physics ticks
+    # occur before a frame .. the data must flow!
+    if _TestFlags(SF_DIRTY):
+        _RefreshTransform()
 
-	_SetFlags(SF_DIRTY)
-	pass
+    _SetFlags(SF_DIRTY)
+    pass
 
 func _LerpBasis(var from : Basis, var to : Basis, var f : float)->Basis:
-	var res : Basis = Basis()
-	res.x = from.x.linear_interpolate(to.x, f)
-	res.y = from.y.linear_interpolate(to.y, f)
-	res.z = from.z.linear_interpolate(to.z, f)
-	return res
+    var res : Basis = Basis()
+    res.x = from.x.linear_interpolate(to.x, f)
+    res.y = from.y.linear_interpolate(to.y, f)
+    res.z = from.z.linear_interpolate(to.z, f)
+    return res
 
 func _SetFlags(var f):
-	flags |= f
+    flags |= f
 
 func _ClearFlags(var f):
-	flags &= ~f
+    flags &= ~f
 
 func _TestFlags(var f):
-	return (flags & f) == f
+    return (flags & f) == f
 
 func _ChangeFlags(var f, var bSet):
-	if bSet:
-		_SetFlags(f)
-	else:
-		_ClearFlags(f)
+    if bSet:
+        _SetFlags(f)
+    else:
+        _ClearFlags(f)
